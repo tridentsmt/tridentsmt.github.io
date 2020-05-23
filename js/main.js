71676a794a1f0347eefb7864c0cf6cb4f04e88d6
tridentsmt <tridentsm@protonmail.com>
2020-05-23 13:14:59 +0100

    add popup ratings.

--- js/main.js
+++ js/main.js
@@ -27,8 +27,49 @@ AOS.init();
         $('html, body').animate({scrollTop: $(this.hash).offset().top - 75}, 1000);
         return false;
     });
-
+    
     setInterval(tick, pause);
 
+    $('#close-popup').on('click', function() {
+        $(this).parent().parent().parent().parent().remove();
+        return false;
+    });
 })(jQuery);
+
+(function($, viewport){
+
+    // Executes only in XS breakpoint
+    if( viewport.is('<lg') ) {
+
+        $('#close-popup').parent().parent().parent().parent().remove();
+        return false;
+    }
+
+    // // Executes in SM, MD and LG breakpoints
+    // if( viewport.is('>=sm') ) {
+    //     // ...
+    // }
+
+    // // Executes in XS and SM breakpoints
+    // if( viewport.is('<md') ) {
+    //     // ...
+    // }
+
+    // // Execute only after document has fully loaded
+    // $(document).ready(function() {
+    //     if( viewport.is('xs') ) {
+    //         // ...
+    //     }
+    // });
+
+    // // Execute code each time window size changes
+    // $(window).resize(
+    //     viewport.changed(function() {
+    //         if( viewport.is('xs') ) {
+    //             // ...
+    //         }
+    //     })
+    // ); 
+
+})(jQuery, ResponsiveBootstrapToolkit);
 
